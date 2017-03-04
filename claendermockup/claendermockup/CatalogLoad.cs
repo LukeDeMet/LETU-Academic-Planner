26d805936b5c5dbde65f04b28d7bf1f70e736d16
Corey Naas <coreynaas@outlook.com>
2017-03-03 20:28:45 -0600

    Better external node stuff

--- claendermockup/claendermockup/CatalogLoad.cs
+++ claendermockup/claendermockup/CatalogLoad.cs
@@ -8,19 +8,31 @@ namespace claendermockup
         public static TreeNode Load()
         {
 
-            TreeNode node2 = new TreeNode("DON'T DO IT");
-            TreeNode node3 = new TreeNode("UNLESS YOUR HATE YOURSELF");
-            TreeNode[] array = new TreeNode[] { node2, node3 };
-            TreeNode BSMENode = new TreeNode("Engineering, Mechanical", array);
-
             TreeNode cosc_cst = new TreeNode("COSC");
             TreeNode eetc_cst = new TreeNode("EETC");
-            TreeNode[] CST = new TreeNode[] { cosc_cst, eetc_cst };
+            TreeNode[] CST = new TreeNode[] { cosc_cst, eetc_cst};
             TreeNode CSTNode = new TreeNode("Computer Science & Engineering Technology", CST);
 
-            return
+            //College and Catalog Node
+            TreeNode LETUNode = new TreeNode("LeTourneau University, Spring 2017 Catalog");
 
-            CSTNode;
+            //Offered Major Nodes
+            TreeNode major_CST = new TreeNode("Computer Science & Engineering Technology");
+
+            TreeNode dept_COSC = new TreeNode("COSC");
+            major_CST.Nodes.Add(dept_COSC);
+
+            TreeNode COSC_1303 = new TreeNode("Computer Science I");
+            dept_COSC.Nodes.Add(COSC_1303);
+
+            TreeNode EETC = new TreeNode("EETC");
+            major_CST.Nodes.Add(EETC);
+
+
+            //add majors to catalog
+            LETUNode.Nodes.Add(major_CST);
+
+            return LETUNode;
         }
     }
 }
--- claendermockup/claendermockup/Form1.cs
+++ claendermockup/claendermockup/Form1.cs
@@ -28,18 +28,20 @@ namespace claendermockup
             treeView1.Nodes.Add(CatalogLoad.Load());
         }
 
+
         //allows dragging from treeview.  
         private void treeView1_ItemDrag(object sender, ItemDragEventArgs e)
         {
             DoDragDrop(e.Item.ToString(), DragDropEffects.Copy);
         }
 
-        //allows the drag to enter in the listbox
         private void listBox1_DragEnter(object sender, DragEventArgs e)
         {
             e.Effect = DragDropEffects.Copy;
         }
 
+        //Start of ListBox1
+        //******************************************************************************************************************
         //adds items to be dropped in listbox
         private void listBox1_DragDrop(object sender, DragEventArgs e)
         {
@@ -49,40 +51,6 @@ namespace claendermockup
                     return;
                 else
                     listBox1.Items.Add((System.String)e.Data.GetData(typeof(System.String)));
-            }
-        }
-
-        //adds items to be dropped in listbox
-        private void listBox2_DragDrop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(typeof(System.String)))
-
-            {
-                listBox2.Items.Add((System.String)e.Data.GetData(typeof(System.String)));
-            }
-        }
-
-        //allows the drag to enter in the listbox
-        private void listBox2_DragEnter(object sender, DragEventArgs e)
-        {
-            e.Effect = DragDropEffects.Copy;
-        }
-
-        //keeps the event going as we drag from one listbox to the other
-        private void listBox9_DragOver(object sender, DragEventArgs e)
-        {
-            e.Effect = DragDropEffects.All;
-        }
-
-        //allows items to be dropped in listbox
-        private void listBox9_DragDrop(object sender, DragEventArgs e)
-        {
-            if (e.Data.GetDataPresent(DataFormats.StringFormat))
-            {
-                string str = (string)e.Data.GetData(DataFormats.StringFormat);
-                listBox9.Items.Clear();
-                listBox9.Items.Add(str);
-                
             }
         }
 
@@ -104,7 +72,7 @@ namespace claendermockup
             }
             else
                 return;
-            
+
             DragDropEffects dde1 = DoDragDrop(s, DragDropEffects.All);
 
             if (dde1 == DragDropEffects.All)
@@ -112,5 +80,297 @@ namespace claendermockup
                 listBox1.Items.RemoveAt(listBox1.IndexFromPoint(e.X, e.Y));
             }
         }
+        //*******************************************************************************************************************
+        //end of ListBox1
+
+        //ListBox2
+        //*******************************************************************************************************************
+        private void listBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                listBox2.Items.Add((System.String)e.Data.GetData(typeof(System.String)));
+            }
+        }
+
+        private void listBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            string s = "";
+            if (listBox2.Items.Count == 0)
+            {
+                return;
+            }
+            int index = listBox2.IndexFromPoint(e.X, e.Y);
+            if (index >= 0)
+            {
+                {
+                    if (listBox2.Items[index].ToString() == null)
+                        s = listBox2.Items[index].ToString();
+                }
+            }
+            else
+                return;
+
+            DragDropEffects dde2 = DoDragDrop(s, DragDropEffects.All);
+
+            if (dde2 == DragDropEffects.All)
+            {
+                listBox2.Items.RemoveAt(listBox2.IndexFromPoint(e.X, e.Y));
+            }
+        }
+
+        //*******************************************************************************************************************
+        //end of listBox2
+
+        //Start of ListBox3
+        //*******************************************************************************************************************
+        private void listBox3_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                listBox3.Items.Add((System.String)e.Data.GetData(typeof(System.String)));
+            }
+        }
+
+        private void listBox3_MouseDown(object sender, MouseEventArgs e)
+        {
+            string s = "";
+            if (listBox3.Items.Count == 0)
+            {
+                return;
+            }
+            int index = listBox3.IndexFromPoint(e.X, e.Y);
+            if (index >= 0)
+            {
+                {
+                    if (listBox3.Items[index].ToString() == null)
+                        s = listBox3.Items[index].ToString();
+                }
+            }
+            else
+                return;
+
+            DragDropEffects dde3 = DoDragDrop(s, DragDropEffects.All);
+
+            if (dde3 == DragDropEffects.All)
+            {
+                listBox3.Items.RemoveAt(listBox3.IndexFromPoint(e.X, e.Y));
+            }
+        }
+        //End of ListBox3
+        //*******************************************************************************************************************
+
+        //Start of ListBox4
+        //*******************************************************************************************************************
+        private void listBox4_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                listBox4.Items.Add((System.String)e.Data.GetData(typeof(System.String)));
+            }
+        }
+
+        private void listBox4_MouseDown(object sender, MouseEventArgs e)
+        {
+            string s = "";
+            if (listBox4.Items.Count == 0)
+            {
+                return;
+            }
+            int index = listBox4.IndexFromPoint(e.X, e.Y);
+            if (index >= 0)
+            {
+                {
+                    if (listBox4.Items[index].ToString() == null)
+                        s = listBox4.Items[index].ToString();
+                }
+            }
+            else
+                return;
+
+            DragDropEffects dde4 = DoDragDrop(s, DragDropEffects.All);
+
+            if (dde4 == DragDropEffects.All)
+            {
+                listBox4.Items.RemoveAt(listBox4.IndexFromPoint(e.X, e.Y));
+            }
+        }
+
+        //End of ListBox4
+        //*******************************************************************************************************************
+
+        //start of ListBox5
+        //*******************************************************************************************************************
+        private void listBox5_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                listBox5.Items.Add((System.String)e.Data.GetData(typeof(System.String)));
+            }
+        }
+
+        private void listBox5_MouseDown(object sender, MouseEventArgs e)
+        {
+            string s = "";
+            if (listBox5.Items.Count == 0)
+            {
+                return;
+            }
+            int index = listBox5.IndexFromPoint(e.X, e.Y);
+            if (index >= 0)
+            {
+                {
+                    if (listBox5.Items[index].ToString() == null)
+                        s = listBox5.Items[index].ToString();
+                }
+            }
+            else
+                return;
+
+            DragDropEffects dde5 = DoDragDrop(s, DragDropEffects.All);
+
+            if (dde5 == DragDropEffects.All)
+            {
+                listBox5.Items.RemoveAt(listBox5.IndexFromPoint(e.X, e.Y));
+            }
+        }
+        //end of ListBox5
+        //*******************************************************************************************************************
+
+        //start of ListBox6
+        //*******************************************************************************************************************
+        private void listBox6_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                listBox6.Items.Add((System.String)e.Data.GetData(typeof(System.String)));
+            }
+        }
+
+        private void listBox6_MouseDown(object sender, MouseEventArgs e)
+        {
+            string s = "";
+            if (listBox6.Items.Count == 0)
+            {
+                return;
+            }
+            int index = listBox6.IndexFromPoint(e.X, e.Y);
+            if (index >= 0)
+            {
+                {
+                    if (listBox6.Items[index].ToString() == null)
+                        s = listBox6.Items[index].ToString();
+                }
+            }
+            else
+                return;
+
+            DragDropEffects dde6 = DoDragDrop(s, DragDropEffects.All);
+
+            if (dde6 == DragDropEffects.All)
+            {
+                listBox6.Items.RemoveAt(listBox6.IndexFromPoint(e.X, e.Y));
+            }
+        }
+        //end of ListBox6
+        //*******************************************************************************************************************
+
+        //start of ListBox7
+        //*******************************************************************************************************************
+        private void listBox7_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                listBox7.Items.Add((System.String)e.Data.GetData(typeof(System.String)));
+            }
+        }
+
+        private void listBox7_MouseDown(object sender, MouseEventArgs e)
+        {
+            string s = "";
+            if (listBox7.Items.Count == 0)
+            {
+                return;
+            }
+            int index = listBox7.IndexFromPoint(e.X, e.Y);
+            if (index >= 0)
+            {
+                {
+                    if (listBox7.Items[index].ToString() == null)
+                        s = listBox7.Items[index].ToString();
+                }
+            }
+            else
+                return;
+
+            DragDropEffects dde7 = DoDragDrop(s, DragDropEffects.All);
+
+            if (dde7 == DragDropEffects.All)
+            {
+                listBox7.Items.RemoveAt(listBox7.IndexFromPoint(e.X, e.Y));
+            }
+        }
+        //end of ListBox7
+        //*******************************************************************************************************************
+
+        //start of ListBox8
+        //*******************************************************************************************************************
+        private void listBox8_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(System.String)))
+            {
+                listBox8.Items.Add((System.String)e.Data.GetData(typeof(System.String)));
+            }
+        }
+
+        private void listBox8_MouseDown(object sender, MouseEventArgs e)
+        {
+            string s = "";
+            if (listBox8.Items.Count == 0)
+            {
+                return;
+            }
+            int index = listBox8.IndexFromPoint(e.X, e.Y);
+            if (index >= 0)
+            {
+                {
+                    if (listBox8.Items[index].ToString() == null)
+                        s = listBox8.Items[index].ToString();
+                }
+            }
+            else
+                return;
+
+            DragDropEffects dde8 = DoDragDrop(s, DragDropEffects.All);
+
+            if (dde8 == DragDropEffects.All)
+            {
+                listBox8.Items.RemoveAt(listBox8.IndexFromPoint(e.X, e.Y));
+            }
+        }
+
+        //end of ListBox8
+        //*******************************************************************************************************************
+
+        //Start of ListBox9
+        //*******************************************************************************************************************
+        private void listBox9_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        //allows items to be dropped in listbox
+        private void listBox9_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string str = (string)e.Data.GetData(DataFormats.StringFormat);
+                listBox9.Items.Clear();
+                listBox9.Items.Add(str);
+
+            }
+        }
+        //end of ListBox9
+        //*******************************************************************************************************************
     }
 }
